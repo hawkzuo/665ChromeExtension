2489cedc07e425f85018fe7a33ca3020a437268f
Jianyu Zuo <zuowings@gmail.com>
2018-04-13 14:13:49 -0500

    Update Chrome Extension

--- gmail-extension/src/extension.js
+++ gmail-extension/src/extension.js
@@ -27,7 +27,8 @@ gmail.observe.on("load", () => {
         const mailBody = currentMail.body();
         const emailContent = currentMail.id_element.text();
 
-        console.log(emailContent);
+        // console.log(emailContent);
+
         // An XHR request will be sent to server to verify the mailBody to be spam or not
         // On callback, update the UI to show the highlights
 
@@ -48,13 +49,35 @@ gmail.observe.on("load", () => {
           success: function(data_back) {                               // Show content
             if (data_back['status'] === 'Failed') {
               currentMail.body('<h1>Server cannot classify example !</h1>' + mailBody);
-            } else if (data_back['status'] === 'Spam'){
-              currentMail.body('<h1 class="label">Potential Spam!</h1>' + mailBody);
+            } else if (data_back['status'] === 'spam'){
+              currentMail.body('<div><h1 class="label">Potential Spam!</h1><button id="showAnyway" class="show">Show anyway</button></div>' );
+
+              $('#showAnyway').click(function (e) {
+                console.log(e.target);
+                currentMail.body('<div><h1 class="label">Potential Spam!</div>' + mailBody)
+              });
             } else {
               // Ham
               // Call Word List API
               // Comment is short, loop over comment words, excahnge
               // Independent to comments
+              currentMail.body('<h1 class="label">Not Spam!</h1>' + mailBody);
+
+              // Keep making AJAX calls
+              $.getJSON( "http://localhost:8000/spam/words", function( data ) {
+                // var items = [];
+                // $.each( data, function( key, val ) {
+                //   items.push( "<li id='" + key + "'>" + val + "</li>" );
+                // });
+                //
+                // $( "<ul/>", {
+                //   "class": "my-new-list",
+                //   html: items.join( "" )
+                // }).appendTo( "body" );
+                console.log(data)
+              });
+
+
             }
             console.log(currentMail);
           },
